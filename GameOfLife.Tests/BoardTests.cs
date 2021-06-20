babdfd69ba5e49174890eed68aa76e89b16dd406
Petutschnig Ewald (BCC.IT-EW.BAS) <ewald.petutschnig@oebb.at>
2021-06-20 22:27:25 +0200

    red: A_board_contains_cells_in_2_dimensions

---
+++ GameOfLife.Tests/BoardTests.cs
@@ -0,0 +1,12 @@
+﻿namespace GameOfLife.Tests
+{
+    public class BoardTests
+    {
+        [ReadableFact]
+        public void A_board_contains_cells_in_2_dimensions()
+        {
+            var board = new Board();
+
+        }
+    }
+}
