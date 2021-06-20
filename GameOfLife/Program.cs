31739823dbe1f1b9441411a15375398152e6f2d5
Petutschnig Ewald (BCC.IT-EW.BAS) <ewald.petutschnig@oebb.at>
2021-06-21 01:14:06 +0200

    console app

---
+++ GameOfLife/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var generation = 1;
+            var board = Board.Parse(
+                "x x x x x x x o x o o o x x o o o x x x x x x x x x x x x x x x x x x x x x x x ",
+                "x x o x x x x x x x o x x x x x x x o x x x x x x x o x x x x x x x o x x x x x ",
+                "x x x o o o x x x x x o o o x x x x x o o o x x x x x o o o x x x x x o o o x x ",
+                "x x x o x o x x x x x o x o x x x x x o x o x x x x x o x o x x x x x o x o x x ",
+                "x x o x o o x x x x o x o o x x x x o x o o x x x x o x o o x o x x o x o o x x ",
+                "x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x ",
+                "x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x ",
+                "x x o x x x x x x x o x x x x x x x o x x x x x x x o x x x x x x x o x x x x x ",
+                "x x x o o o x x x x x o o o x x x x x o o o x x x x x o o o x x x x x o o o x x ",
+                "x x x o x o x x x x x o x o x x x x x o x o x x x x x o x o x x x x x o x o x x ",
+                "x x o x o o x x x x o x o o x x x x o x o o x x x x o x o o x x x x o x o o x x ",
+                "x x x x x x x x x x x x x x x x o x x x x x x x x x x x x x x x x x x x x x x x ",
+                "x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x ",
+                "x x o x x x x x x x o x x x x x x x o x x x x x x x o x x x x x x x o x x x x x ",
+                "x x x o o o x x x x x o o o x x x x x o o o x x x x x o o o x x x x x o o o x x ",
+                "x x x o x o x x x x x o x o x x x x x o x o x x x x x o x o x x x x x o x o x o ",
+                "x x o x o o x x x x o x o o x x x x o x o o x x x x o x o o x x x x o x o o x x ",
+                "o x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x o x x x o ",
+                "o x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x o x ",
+                "x x o x x x x x x x o x x x x x x o o x x o x x x x o x x x x x x x o x x x x x ",
+                "x x x o o o x x x x x o o o x x x x x o o o x x x x x o o o x x x x o o o o x x ",
+                "x x x o x o x x x x x o x o x x x x x o x o x x x x x o x o x x x x x o x o x x ",
+                "x x o x o o x x x x o x o o x x x x o x o o x x x x o x o o x x o x o x o o x x ",
+                "x x x x x x x x x x x x x x x x x x x x x x x x x x o x x x x x x x x x x x x x "
+                );
+
+            while(true)
+            {
+                var screen = board.ToString();
+                board = board.NextGeneration();
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(screen);
+                Console.WriteLine(generation);
+                await Task.Delay(10);
+                generation++;
+            }
+        }
+    }
+}
