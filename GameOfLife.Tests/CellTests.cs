2d91cba996b271193610f1d1ed3f58345cfb40fa
Petutschnig Ewald (BCC.IT-EW.BAS) <ewald.petutschnig@oebb.at>
2021-06-20 21:53:14 +0200

    red: a cell can be alive

--- GameOfLife.Tests/CellTests.cs
+++ GameOfLife.Tests/CellTests.cs
@@ -10,5 +10,12 @@ namespace GameOfLife.Tests
             var cell = new Cell();
             cell.IsDead.ShouldBe(true);
         }
+
+        [ReadableFact]
+        public void A_cell_can_be_alive()
+        {
+            var cell = new Cell();
+            cell.IsDead.ShouldBe(false);
+        }
     }
 }
