c011d5ee6109b8bb5e163c2594c103bba948f19e
Petutschnig Ewald (BCC.IT-EW.BAS) <ewald.petutschnig@oebb.at>
2021-06-20 22:33:23 +0200

    green: A_board_contains_cells_in_2_dimensions

---
+++ GameOfLife.Tests/Board.cs
@@ -0,0 +1,24 @@
+﻿namespace GameOfLife.Tests
+{
+    public class Board
+    {
+        public Cell[,] Cells { get; }
+
+        public Board(int width, int height)
+        {
+            Cells = new Cell[width, height];
+            InitCells(width, height);
+        }
+
+        private void InitCells(int width, int height)
+        {
+            for (var row = 0; row < height; row++)
+            {
+                for (var col = 0; col < width; col++)
+                {
+                    Cells[row, col] = new Cell();
+                }
+            }
+        }
+    }
+}
--- GameOfLife.Tests/BoardTests.cs
+++ GameOfLife.Tests/BoardTests.cs
@@ -1,12 +1,16 @@
-﻿namespace GameOfLife.Tests
+﻿using Shouldly;
+
+namespace GameOfLife.Tests
 {
     public class BoardTests
     {
         [ReadableFact]
         public void A_board_contains_cells_in_2_dimensions()
         {
-            var board = new Board();
+            var board = new Board(width: 200, height: 200);
 
+            board.Cells[0, 0].ShouldNotBeNull();
+            board.Cells[199, 199].ShouldNotBeNull();
         }
     }
 }
